7f9ad0b4e4461a5596a28895d5715f8f246b7da4
basiltroo <basil@troo.earth>
2025-12-31 17:41:47 +0800

    Cleaned Up unecessary tables

--- src/modules/project/projectRoutes.js
+++
@@ -1,8 +0,0 @@
-const {getAllProjectsController, findProjectByIdController} = require("./projectController");
-const router = require('express').Router();
-
-router.get('/', getAllProjectsController);
-router.get('/:id', findProjectByIdController);
-// router.post('/:id/upload-docs', uploadProjectDocsController);
-
-module.exports = router;

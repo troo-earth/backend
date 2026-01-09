51b12ec3188da7ac1768231c1ac7841779c951d7
basiltroo <basil@troo.earth>
2026-01-09 16:15:36 +0800

    Minor fixes to Session

--- src/config/session.js
+++ src/config/session.js
@@ -3,6 +3,10 @@ const { RedisStore } = require('connect-redis');
 const redisClient = require('./redis');
 
 const isProd = process.env.NODE_ENV === 'production';
+const allowCrossSiteDev = process.env.ALLOW_CROSS_SITE_DEV === 'true';
+
+const sameSite =
+  allowCrossSiteDev ? 'none' : 'lax';
 
 const sessionMiddleware = session({
   name: 'troo.sid',
@@ -13,16 +17,17 @@ const sessionMiddleware = session({
   }),
 
   secret: process.env.SESSION_SECRET,
+
   resave: false,
   saveUninitialized: false,
 
   cookie: {
     httpOnly: true,
-    secure: true,        // REQUIRED for SameSite=None
-    sameSite: 'none',    // REQUIRED for localhost → api.troo.earth
-    domain: '.troo.earth',
+    secure: false,
+    sameSite: 'lax',
     maxAge: 1000 * 60 * 60 * 24
-  },
+  }
+  ,
 });
 
 module.exports = sessionMiddleware;

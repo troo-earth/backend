cdd7abf00f14db437fac483ebaa6245b91b0b2cf
basiltroo <basil@troo.earth>
2026-02-08 16:24:36 +0800

    Added Authorization Protection to Trading

--- src/modules/trading/tradingRoutes.js
+++ src/modules/trading/tradingRoutes.js
@@ -6,14 +6,16 @@ const {
     transferCreditsController,
     retireCreditsController
 } = require('./tradingController');
+const authorizePermission = require('../../middleware/authorizePermission');
+const { PERMISSIONS } = require('../../constants/permissions');
 
 // Route for buying credits
-router.post('/buy-credits', buyCreditsController);
+router.post('/buy-credits', authorizePermission(PERMISSIONS.BUY_CREDITS), buyCreditsController);
 // Route for selling credits
-router.post('/sell-credits', sellCreditsController);
+router.post('/sell-credits', authorizePermission(PERMISSIONS.SELL_CREDITS), sellCreditsController);
 // Route for retiring credits
-router.post('/retire-credits', retireCreditsController);
+router.post('/retire-credits', authorizePermission(PERMISSIONS.RETIRE_CREDITS), retireCreditsController);
 // Route for transferring credits
-router.post('/transfer-credits', transferCreditsController);
+router.post('/transfer-credits', authorizePermission(PERMISSIONS.TRANSFER_CREDITS), transferCreditsController);
 
 module.exports = router;

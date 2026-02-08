963ad0835e95191b0a8bfa15f42d895e570c0552
basiltroo <basil@troo.earth>
2026-02-08 16:19:52 +0800

    Added ROles, Permissions and Middleware

---
+++ src/constants/permissions.js
@@ -0,0 +1,21 @@
+const PERMISSIONS = {
+  VIEW_DATA: 'view_data',
+
+  BUY_CREDITS: 'buy_credits',
+  SELL_CREDITS: 'sell_credits',
+  RETIRE_CREDITS: 'retire_credits',
+  TRANSFER_CREDITS: 'transfer_credits',
+
+  CREATE_LISTING: 'create_listing',
+  EDIT_LISTING: 'edit_listing',
+  CANCEL_LISTING: 'cancel_listing',
+
+  INVITE_USER: 'invite_user',
+  REMOVE_USER: 'remove_user',
+  ASSIGN_ROLE: 'assign_role',
+  MANAGE_ADMINS: 'manage_admins',
+
+  UPDATE_ORG: 'update_org',
+};
+
+module.exports = { PERMISSIONS };
---
+++ src/constants/roles.js
@@ -0,0 +1,8 @@
+const ROLES = {
+  SUPERADMIN: 'superadmin',
+  ADMIN: 'admin',
+  MANAGER: 'manager',
+  VIEWER: 'viewer',
+};
+
+module.exports = { ROLES };
